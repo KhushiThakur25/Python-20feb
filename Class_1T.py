4eca123bf49e6e5ada4caa2b881e7ce510c234a1
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2024-02-20 18:01:24 +0530

    Add files via upload

---
+++ Class_1T.py
@@ -0,0 +1,44 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import keyword
+print(keyword.kwlist)
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+import = 50
+SyntaxError: invalid syntax
+name = "Khushi"
+toy name = "teddy bear"
+SyntaxError: invalid syntax
+toy_name = "teddy bear"
+a = 50
+b = 10
+a + b
+60
+a - b
+40
+>>> a * b
+500
+>>> a/b
+5.0
+>>> a ** b
+97656250000000000
+>>> a % b
+0
+>>> a // b
+5
+>>> a = a + 10
+>>> a
+60
+>>> b += 20
+>>> b
+30
+>>> a *= 2
+>>> a
+120
+>>> 8 & 22
+0
+>>> 13 & 17
+1
+>>> 9 | 10
+11
+>>> 13 | 17
+29
