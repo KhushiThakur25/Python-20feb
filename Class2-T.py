9af406d59e7667ed0adf8366cd486aafad663007
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2024-02-22 10:51:14 +0530

    Add files via upload

---
+++ Class2-T.py
@@ -0,0 +1,61 @@
+
+'''a = 5
+b = 6
+print("This is a python language..",end = " ")
+print(a,b,end = " ")
+print("Sum of a and b is:",a+b)'''
+
+#There is a twinkle twinkle little star poem
+'''sdgfr
+fgnghmjh
+ghjgh'''
+
+'''print("""Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+
+Then the trav'ller in the dark,
+Thanks you for your tiny spark,
+He could not see which way to go,
+If you did not twinkle so.
+
+In the dark blue sky you keep,
+And often thro' my curtains peep,
+For you never shut your eye,
+Till the sun is in the sky.""")'''
+
+'''x = 91
+y = 8
+print("Sum of",x,"and",y,"is:",x+y)
+
+print("Sum of {} and {} is:{}".format(x,y,x+y))
+
+print(f"sum of {x} and {y} is:{x+y}")
+
+#input("Enter the value..")--it return string value
+value1 = int(input("Enter the value.."))
+value2 = int(input("Enter the value.."))
+print(value1 + value2)'''
+
+'''num = int(input("Enter the number.."))
+
+if num%2 == 0:
+    print("Number is Even")
+else:
+    print("Number is odd")'''
+
+
+a = 56
+b = 211
+if a>b:
+    print("a is greater")
+else:
+    print("b is greater")
+
+
