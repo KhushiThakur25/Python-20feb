866b7c71df7241d1627d776bc40fe3777326b22d
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2024-02-27 10:57:48 +0530

    Add files via upload

---
+++ class_5T.py
@@ -0,0 +1,47 @@
+#factorial prgram
+
+'''num = int(input("Enter the value to get factorial value.."))
+product = 1
+for i in range(1,num+1):
+    product *= i
+
+print("The factorial is..",product)'''
+
+
+#fibonacci series - 0 1 1 2 3 5 8 13 21......
+
+'''d_1 = 0
+d_2 = 1
+
+num = int(input("Enter the terms.."))
+print(d_1 , d_2 ,end = " ")
+for i in range(2,num+1):
+    
+    d_3 = d_1 + d_2
+    print(d_3,end = " ")
+    d_1,d_2 = d_2,d_3'''
+#reverse the number
+
+num = int(input("Enter the number.."))
+rev = 0
+while num > 0:
+    rem = num%10
+    rev = rev*10 + rem
+    num//=10
+print(rev)
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  
+    
+    
