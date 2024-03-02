927a7f47715be498aeeae8f397bd265fa4e80386
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2024-03-02 11:06:15 +0530

    Add files via upload

--- class_5T.py
+++ class_5T.py
@@ -18,18 +18,37 @@ print(d_1 , d_2 ,end = " ")
 for i in range(2,num+1):
     
     d_3 = d_1 + d_2
+    
     print(d_3,end = " ")
-    d_1,d_2 = d_2,d_3'''
+    
+    d_1 = d_2
+    d_2 = d_3'''
+    
 #reverse the number
 
-num = int(input("Enter the number.."))
+'''num = int(input("Enter the number.."))
 rev = 0
 while num > 0:
     rem = num%10
     rev = rev*10 + rem
     num//=10
-print(rev)
+print(rev)'''
 
+#Prime number
+
+num = int(input("Enter the number.."))
+prime = False
+for i in range(2,num//2+1):
+    if num%i == 0:
+        prime = False
+        break
+    else:
+        prime = True
+if prime:
+    print("number is prime")
+else:
+    print("number is not prime")
+        
 
 
 
