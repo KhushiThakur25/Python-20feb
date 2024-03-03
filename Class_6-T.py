a0cd865ecabd969bb9e5f47668c2e00dbe7bd555
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2024-03-03 10:55:47 +0530

    Add files via upload

---
+++ Class_6-T.py
@@ -0,0 +1,40 @@
+for i in range(5):
+    for j in range(5):
+        print("*",end = " ")
+    print()
+
+print("--------------------------------")
+
+''' *
+    * *
+    * * *
+    * * * *
+    * * * * * '''
+
+for i in range(5):
+    for j in range(i+1):
+        print("*",end = " ")
+    print()
+
+print("--------------------------------")
+    
+''' * * * * *
+    * * * *
+    * * *
+    * *
+    *  '''
+
+for i in range(5):
+    for j in range(5-i):
+        print("*",end = " ")
+    print()
+
+print("--------------------------------")
+
+
+for i in range(5):
+    for j in range(i):
+        print(" ",end = " ")
+    for k in range(i,5):
+        print("*",end = " ")
+    print()
